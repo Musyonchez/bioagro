610646addbd9cf6cbb3f01e12d2a3cf927685a92
Musyoka Philip <87429036+Musyonchez@users.noreply.github.com>
2023-11-06 18:49:50 +0300

    Update Navbar.js

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,17 +1,13 @@
 "use client"
 
 
-
-
-
 import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { FaBars } from 'react-icons/fa'; // Import the hamburger menu icon
 
 const Navbar = () => {
-
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-
 
   const toggleDropdown = () => {
     setDropdownOpen(!isDropdownOpen);
@@ -31,62 +27,66 @@ const Navbar = () => {
     marginRight: '20px',
   };
 
+  const menuIconStyles = {
+    fontSize: '2rem',
+    color: '#000',
+    cursor: 'pointer',
+  };
+
   return (
     <div>
-      <div style={menuStyles} className=' justify-evenly px-10'>
+      <div style={menuStyles} className='justify-between px-10'>
         <Link href="/">
           <Image src="/images/logo.png" alt="Logo" width={150} height={50} />
         </Link>
 
+        {/* Hamburger Menu Icon */}
+        <FaBars style={menuIconStyles} onClick={toggleDropdown} />
 
         <div
-            style={{ position: 'relative' }}
-            onMouseEnter={toggleDropdown}
-            onMouseLeave={toggleDropdown}
-          >
-            <a href="/services" style={linkStyles} className="hover:text-green-400">
-              Services
-            </a>
-            {isDropdownOpen && (
-              <div
-                style={{
-                  position: 'absolute',
-                  top: '100%',
-                  left: 0,
-                  backgroundColor: '#fff',
-                  border: '1px solid #ccc',
-                  flexDirection: 'column',
-                }}
-              >
-                <Link href="/" style={linkStyles}>
-              Home
-            </Link>
-            <Link href="/aboutus" style={linkStyles} className="hover:text-green-400">
-              About
-            </Link>
-            <Link href="/V&F" style={linkStyles} className="hover:text-green-400">
-              V&F
-            </Link>
-            <Link href="/dairy" style={linkStyles} className="hover:text-green-400">
-              Dairy
-            </Link>
-            <Link href="/seedlings" style={linkStyles} className="hover:text-green-400">
-              Seedlings
-            </Link>
-            <Link href="/projects" style={linkStyles} className="hover:text-green-400">
-              Projects
-            </Link>
-            <Link href="/products" style={linkStyles} className="hover:text-green-400">
-              Products
-            </Link>
-            <Link href="/contact" style={linkStyles} className="hover:text-green-400">
-              Contact
-            </Link>
-              </div>
-            )}
-          </div>
-
-
+          style={{ position: 'relative' }}
+          onMouseEnter={toggleDropdown}
+          onMouseLeave={toggleDropdown}
+        >
+          {/* If the dropdown is open, render the links */}
+          {isDropdownOpen && (
+            <div
+              style={{
+                position: 'absolute',
+                top: '100%',
+                left: 0,
+                backgroundColor: '#fff',
+                border: '1px solid #ccc',
+                flexDirection: 'column',
+              }}
+            >
+              <Link href="/" style={linkStyles}>
+                Home
+              </Link>
+              <Link href="/aboutus" style={linkStyles} className="hover:text-green-400">
+                About
+              </Link>
+              <Link href="/V&F" style={linkStyles} className="hover:text-green-400">
+                V&F
+              </Link>
+              <Link href="/dairy" style={linkStyles} className="hover:text-green-400">
+                Dairy
+              </Link>
+              <Link href="/seedlings" style={linkStyles} className="hover:text-green-400">
+                Seedlings
+              </Link>
+              <Link href="/projects" style={linkStyles} className="hover:text-green-400">
+                Projects
+              </Link>
+              <Link href="/products" style={linkStyles} className="hover:text-green-400">
+                Products
+              </Link>
+              <Link href="/contact" style={linkStyles} className="hover:text-green-400">
+                Contact
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
