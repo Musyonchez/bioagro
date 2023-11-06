fe8dfe609506848fafd8405b68b4ed2859710242
Musyoka Philip <87429036+Musyonchez@users.noreply.github.com>
2023-11-06 16:37:36 +0300

    Updated landing page content on new branch

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -9,9 +9,13 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const Navbar = () => {
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
 
 
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
 
   const menuStyles = {
     display: 'flex',
@@ -29,53 +33,32 @@ const Navbar = () => {
 
   return (
     <div>
-      <div style={menuStyles} className='px-10'>
+      <div style={menuStyles} className=' justify-evenly px-10'>
         <Link href="/">
           <Image src="/images/logo.png" alt="Logo" width={150} height={50} />
         </Link>
-        <div className="lg:flex flex-row hidden">
-          <Link href="/" style={linkStyles}>
-            Home
-          </Link>
-          <Link href="/aboutus" style={linkStyles} className=" text-green-400">
-            About
-          </Link>
-          <Link href="/vandf" style={linkStyles} className="hover:text-green-400">
-            V&F
-          </Link>
-          <Link href="/dairy" style={linkStyles} className="hover:text-green-400">
-            Dairy
-          </Link>
-          <Link href="/seedlings" style={linkStyles} className="hover:text-green-400">
-            Seedlings
-          </Link>
-          <Link href="/projects" style={linkStyles} className="hover:text-green-400">
-            Projects
-          </Link>
-          <Link href="/products" style={linkStyles} className="hover:text-green-400">
-            Productsgit checkout -b new-branch-name
-
-          </Link>
-          <Link href="/contact" style={linkStyles} className="hover:text-green-400">
-            Contact
-          </Link>
-        </div>
 
 
-
-
-
-        <div className=" ">
-          <div
-            style={{
-              border: '1px solid #ccc',
-              padding: '20px',
-              display: 'flex',
-              flexDirection: 'column',
-            }}
-            className=' bg-stone-300 justify-end text-end w-1/4 float-right'
+        <div
+            style={{ position: 'relative' }}
+            onMouseEnter={toggleDropdown}
+            onMouseLeave={toggleDropdown}
           >
-            <Link href="/" style={linkStyles}>
+            <a href="/services" style={linkStyles} className="hover:text-green-400">
+              Services
+            </a>
+            {isDropdownOpen && (
+              <div
+                style={{
+                  position: 'absolute',
+                  top: '100%',
+                  left: 0,
+                  backgroundColor: '#fff',
+                  border: '1px solid #ccc',
+                  flexDirection: 'column',
+                }}
+              >
+                <Link href="/" style={linkStyles}>
               Home
             </Link>
             <Link href="/aboutus" style={linkStyles} className="hover:text-green-400">
@@ -99,8 +82,11 @@ const Navbar = () => {
             <Link href="/contact" style={linkStyles} className="hover:text-green-400">
               Contact
             </Link>
+              </div>
+            )}
           </div>
-        </div>
+
+
       </div>
     </div>
   );
