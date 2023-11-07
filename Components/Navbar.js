8e69419b02ee841493ea9cbaf15e22f55aa60e7f
Musyoka Philip <87429036+Musyonchez@users.noreply.github.com>
2023-11-07 09:36:16 +0300

    update

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,92 +1,43 @@
-"use client"
-
-
-import React, { useState } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import { FaBars } from 'react-icons/fa'; // Import the hamburger menu icon
+// Navbar.js
+import React from "react";
+import Link from "next/link";
+import Logo from "./Logo";
+import Button from "./Button";
+import Menu from "./Menu";
 
 const Navbar = () => {
-  const [isDropdownOpen, setDropdownOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!isDropdownOpen);
-  };
-
-  const menuStyles = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: '5px 10px',
-  };
-
-  const linkStyles = {
-    fontSize: '1.5rem',
-    color: '#000',
-    textDecoration: 'none',
-    marginRight: '20px',
-  };
-
-  const menuIconStyles = {
-    fontSize: '2rem',
-    color: '#000',
-    cursor: 'pointer',
-  };
-
   return (
-    <div>
-      <div style={menuStyles} className='justify-between px-10'>
-        <Link href="/">
-          <Image src="/images/logo.png" alt="Logo" width={150} height={50} />
-        </Link>
-
-        {/* Hamburger Menu Icon */}
-        <div style={{ position: 'relative' }}>
-          <FaBars style={menuIconStyles} onClick={toggleDropdown} />
-          {isDropdownOpen && (
-            <div className=' z-50'
-              style={{
-                      position: 'absolute',
-                      paddingRight: '100px',
-                      paddingLeft: '20px',
-                      marginTop: '30px',
-                      marginRight: '30px',
-                      transform: 'translateX(-50%)',
-                      backgroundColor: 'rgb(255, 255, 255)',
-                      border: '1px solid rgb(204, 204, 204)',
-                      flexDirection: 'column',
-                      display: 'flex',
-                    }}
-              >
-              <Link href="/" style={linkStyles}>
-                Home
-              </Link>
-              <Link href="/aboutus" style={linkStyles} className="hover:text-green-400">
-                About
-              </Link>
-             <Link href="/V&F" style={linkStyles} className="hover:text-green-400">
-                V&F
-              </Link>
-              <Link href="/dairy" style={linkStyles} className="hover:text-green-400">
-                Dairy
-              </Link>
-              <Link href="/seedlings" style={linkStyles} className="hover:text-green-400">
-                Seedlings
-              </Link>
-              <Link href="/projects" style={linkStyles} className="hover:text-green-400">
-                Projects
-              </Link>
-              <Link href="/products" style={linkStyles} className="hover:text-green-400">
-                Products
-              </Link>
-              <Link href="/contact" style={linkStyles} className="hover:text-green-400">
-                Contact
-              </Link>
-            </div>
-          )}
+    <>
+      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex justify-between items-center h-full">
+            {/* Logo component */}
+            <Logo />
+            {/* Navigation links */}
+            <ul className="hidden md:flex gap-x-6 text-white">
+              <li>
+                <Link href="/about">
+                  <p>About Us</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/services">
+                  <p>Services</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contacts">
+                  <p>Contacts</p>
+                </Link>
+              </li>
+            </ul>
+            {/* Button component */}
+            <Button />
+            <Menu />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
